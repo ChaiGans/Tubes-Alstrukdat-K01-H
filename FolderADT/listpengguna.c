580c39172ee60dde1afc302f72c99bcd250072f8
ChaiGans <elbertchailes888@gmail.com>
2023-10-29 21:01:33 +0700

    change: liststatik to listpengguna

---
+++ FolderADT/listpengguna.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include "listpengguna.h"
+#include <math.h>
+
+/* ********** KONSTRUKTOR ********** */
+/* Konstruktor : create List kosong  */
+void CreateListPengguna(ListPengguna *l) {
+    /* I.S. l sembarang */
+    /* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
+    /* Proses: Inisialisasi semua elemen List l dengan MARK */
+    int i;
+    for (i=IDX_MIN_STATIK;i<CAPACITY_STATIK;i++) {
+        (*l).contents[i].index = MARK_STATIK;
+    }
+}
+
+/* ********** SELEKTOR (TAMBAHAN) ********** */
+/* *** Banyaknya elemen *** */
+int ListPenggunaLength(ListPengguna l) {
+    /* Mengirimkan banyaknya elemen efektif List */
+    /* Mengirimkan nol jika List kosong */  
+    int i, length;
+    length = 0;
+    for (i=IDX_MIN_STATIK; i<CAPACITY_STATIK; i++) {
+        if (l.contents[i].index != MARK_STATIK) {
+            length += 1;
+        }
+    }
+    return length;
+}
+
+/* *** Selektor INDEKS *** */
+IdxType getFirstIdxListPengguna(ListPengguna l) {
+    /* Prekondisi : List l tidak kosong */
+    /* Mengirimkan indeks elemen l pertama */
+    return IDX_MIN_STATIK;
+}
+
+IdxType getLastIdxListPengguna(ListPengguna l) {
+    /* Prekondisi : List l tidak kosong */
+    /* Mengirimkan indeks elemen l terakhir */
+    return ListPenggunaLength(l)-1;
+}
+
+/* ********** Test Indeks yang valid ********** */
+boolean isIdxValidListPengguna(ListPengguna l, IdxType i) {
+    /* Mengirimkan true jika i adalah indeks yang valid utk kapasitas List l */
+    /* yaitu antara indeks yang terdefinisi utk container*/
+    return i<CAPACITY_STATIK && i>=IDX_MIN_STATIK;
+}
+
+boolean isIdxEffListPengguna(ListPengguna l, IdxType i) {
+    /* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
+    /* yaitu antara 0..length(l)-1 */
+    return i>=IDX_MIN_STATIK && i<ListPenggunaLength(l);
+}
+
+/* ********** TEST KOSONG/PENUH ********** */
+/* *** Test List kosong *** */
+boolean isEmptyListPengguna(ListPengguna l) {
+    /* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
+    return (ListPenggunaLength(l) == 0);
+}
+
+/* *** Test List penuh *** */
+boolean isFullListPengguna(ListPengguna l) {
+    /* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
+    return ListPenggunaLength(l) == CAPACITY_STATIK;
+}
+
+/* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
+/* *** Mendefinisikan isi List dari pembacaan *** */
+
+void printListPengguna(ListPengguna l) {
+    /* Proses : Menuliskan isi List dengan traversal, List ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+    /* I.S. l boleh kosong */
+    /* F.S. Jika l tidak kosong: [e1,e2,...,en] */
+    /* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+    /* Jika List kosong : menulis [] */
+    int i;
+    if (isEmptyListPengguna(l)) {
+        printf("[]");
+    }
+    else {
+        printf("[");
+        for (i=0; i<ListPenggunaLength(l); i++) {
+            if (i==ListPenggunaLength(l)-1) {
+                printf("%s", l.contents[i].username);
+            } else {
+                printf("%s,", l.contents[i].username);
+            }
+        }
+        printf("]");
+    }
+}
+
+/* ********** SEARCHING ********** */
+/* ***  Perhatian : List boleh kosong!! *** */
+int indexOfListPengguna(ListPengguna l, Profile val) {
+    /* Search apakah ada elemen List l yang bernilai val */
+    /* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
+    /* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
+    /* Skema Searching yang digunakan bebas */
+    if (isEmptyListPengguna(l)) {
+        return IDX_UNDEF_STATIK;
+    } else {
+        int i;
+        for (i=IDX_MIN_STATIK; i<ListPenggunaLength(l); i++) {
+            if (l.contents[i].index == val.index) {
+                return i;
+            }
+        }
+        return IDX_UNDEF_STATIK;
+    }
+}
+
+/* ********** MENAMBAH ELEMEN ********** */
+/* *** Menambahkan elemen terakhir *** */
+void insertFirstListPengguna(ListPengguna *l, Profile val) {
+    /* Proses: Menambahkan val sebagai elemen pertama List */
+    /* I.S. List l boleh kosong, tetapi tidak penuh */
+    /* F.S. val adalah elemen pertama l yang baru */
+    /* *** Menambahkan elemen pada index tertentu *** */
+    int i;
+    for (i=ListPenggunaLength(*l)-1; i>=0; i--){
+        (*l).contents[i+1] = (*l).contents[i];
+    }
+    (*l).contents[0] = val;
+}
+
+void insertAtListPengguna(ListPengguna *l, Profile val, IdxType idx) {
+    /* Proses: Menambahkan val sebagai elemen pada index idx List */
+    /* I.S. List l tidak kosong dan tidak penuh, idx merupakan index yang valid di l */
+    /* F.S. val adalah elemen yang disisipkan pada index idx l */
+    int i;
+    for (i = ListPenggunaLength(*l) - 1; i>=idx; i--){
+        (*l).contents[i+1] = (*l).contents[i];
+    }
+    (*l).contents[idx] = val;
+}
+
+/* *** Menambahkan elemen terakhir *** */
+void insertLastListPengguna(ListPengguna *l, Profile val) {
+    /* Proses: Menambahkan val sebagai elemen terakhir List */
+    /* I.S. List l boleh kosong, tetapi tidak penuh */
+    /* F.S. val adalah elemen terakhir l yang baru */
+    (*l).contents[ListPenggunaLength(*l)] = val;
+}
+
+/* ********** MENGHAPUS ELEMEN ********** */
+/* *** Menghapus elemen pertama *** */
+void deleteFirstListPengguna(ListPengguna *l, Profile *val) {
+    /* Proses : Menghapus elemen pertama List */
+    /* I.S. List tidak kosong */
+    /* F.S. val adalah nilai elemen pertama l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+    int i;
+    *val = (*l).contents[0];
+    for (i=0; i<ListPenggunaLength(*l)-1; i++) {
+        (*l).contents[i] = (*l).contents[i+1];
+    }
+    (*l).contents[getLastIdxListPengguna(*l)].index = MARK_STATIK;
+}
+
+/* *** Menghapus elemen pada index tertentu *** */
+void deleteAtListPengguna(ListPengguna *l, Profile *val, IdxType idx) {
+    /* Proses : Menghapus elemen pada index idx List */
+    /* I.S. List tidak kosong, idx adalah index yang valid di l */
+    /* F.S. val adalah nilai elemen pada index idx l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+    int i;
+    *val = (*l).contents[idx];
+    for (i=idx; i<ListPenggunaLength(*l)-1; i++) {
+        (*l).contents[i] = (*l).contents[i+1];
+    }
+    (*l).contents[getLastIdxListPengguna(*l)].index = MARK_STATIK;
+}
+
+/* *** Menghapus elemen terakhir *** */
+void deleteLastListPengguna(ListPengguna *l, Profile *val) {
+    /* Proses : Menghapus elemen terakhir List */
+    /* I.S. List tidak kosong */
+    /* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
+    /*      Banyaknya elemen List berkurang satu */
+    /*      List l mungkin menjadi kosong */
+    *val = (*l).contents[getLastIdxListPengguna(*l)];
+    (*l).contents[getLastIdxListPengguna(*l)].index = MARK_STATIK;
+}
--- FolderADT/listpengguna.h
+++ FolderADT/listpengguna.h
@@ -3,11 +3,12 @@
 /* Penempatan elemen selalu rapat kiri */
 /* Banyaknya elemen didefinisikan secara implisit, memori list statik */
 
-#ifndef LISTSTATIK_H
-#define LISTSTATIK_H
+#ifndef ListPengguna_H
+#define ListPengguna_H
 
 #include "boolean.h"
-
+#include "matrix.h"
+#include "prioqueuechar.h"
 /*  Kamus Umum */
 #define CAPACITY_STATIK 20
 /* Kapasitas penyimpanan */
@@ -19,14 +20,25 @@
 /* Nilai elemen tak terdefinisi*/
 
 /* Definisi elemen dan koleksi objek */
-typedef int ElType;  /* type elemen List */
+typedef struct {
+    int index;
+    char username[20];
+    char password[20]; 
+    char bio[135]; // Bio max 135 karakter
+    int nomorHP; 
+    char weton[6]; // Pahing, Kliwon, Wage, Pon, dan Legi (case insensitive)
+    char status[6]; // PUBLIC, PRIVAT
+    Matrix fotoProfil[5][5]; // use pcolor.c
+    PrioQueueChar permintaanTeman; // harusnya priorqueuepermintaan (check by jumlah teman atau length of listTeman)
+} Profile;
+
 typedef int IdxType;
 typedef struct {
-   ElType contents[CAPACITY_STATIK]; /* memori tempat penyimpan elemen (container) */
-} ListStatik;
+   Profile contents[CAPACITY_STATIK]; /* memori tempat penyimpan elemen (container) */
+} ListPengguna;
 /* Indeks yang digunakan [0..CAPACITY-1] */
-/* Jika l adalah ListStatik, cara deklarasi dan akses: */
-/* Deklarasi : l : ListStatik */
+/* Jika l adalah ListPengguna, cara deklarasi dan akses: */
+/* Deklarasi : l : ListPengguna */
 /* Maka cara akses: 
    ELMT(l,i) untuk mengakses elemen ke-i */
 /* Definisi : 
@@ -34,48 +46,48 @@ typedef struct {
    Definisi elemen pertama: ELMT(l,i) dengan i=0 */
 
 /* ********** SELEKTOR ********** */
-#define ELMT(l, i) (l).contents[(i)]
+// #define ELMT(l, i) (l).contents[(i)]
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create List kosong  */
-void CreateListStatik(ListStatik *l);
+void CreateListPengguna(ListPengguna *l);
 /* I.S. l sembarang */
 /* F.S. Terbentuk List l kosong dengan kapasitas CAPACITY */
 /* Proses: Inisialisasi semua elemen List l dengan MARK */
 
 /* ********** SELEKTOR (TAMBAHAN) ********** */
 /* *** Banyaknya elemen *** */
-int listStatikLength(ListStatik l);
+int ListPenggunaLength(ListPengguna l);
 /* Mengirimkan banyaknya elemen efektif List */
 /* Mengirimkan nol jika List kosong */  
 
 /* *** Selektor INDEKS *** */
-IdxType getFirstIdxListStatik(ListStatik l);
+IdxType getFirstIdxListPengguna(ListPengguna l);
 /* Prekondisi : List l tidak kosong */
 /* Mengirimkan indeks elemen l pertama */
-IdxType getLastIdxListStatik(ListStatik l);
+IdxType getLastIdxListPengguna(ListPengguna l);
 /* Prekondisi : List l tidak kosong */
 /* Mengirimkan indeks elemen l terakhir */
 
 /* ********** Test Indeks yang valid ********** */
-boolean isIdxValidListStatik(ListStatik l, IdxType i);
+boolean isIdxValidListPengguna(ListPengguna l, IdxType i);
 /* Mengirimkan true jika i adalah indeks yang valid utk kapasitas List l */
 /* yaitu antara indeks yang terdefinisi utk container*/
-boolean isIdxEffListStatik(ListStatik l, IdxType i);
+boolean isIdxEffListPengguna(ListPengguna l, IdxType i);
 /* Mengirimkan true jika i adalah indeks yang terdefinisi utk List l */
 /* yaitu antara 0..length(l)-1 */
 
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test List kosong *** */
-boolean isEmptyListStatik(ListStatik l);
+boolean isEmptyListPengguna(ListPengguna l);
 /* Mengirimkan true jika List l kosong, mengirimkan false jika tidak */
 /* *** Test List penuh *** */
-boolean isFullListStatik(ListStatik l);
+boolean isFullListPengguna(ListPengguna l);
 /* Mengirimkan true jika List l penuh, mengirimkan false jika tidak */
 
 /* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
 /* *** Mendefinisikan isi List dari pembacaan *** */
-void readListStatik(ListStatik *l);
+void readListPengguna(ListPengguna *l);
 /* I.S. l sembarang */
 /* F.S. List l terdefinisi */
 /* Proses: membaca banyaknya elemen l dan mengisi nilainya */
@@ -85,7 +97,7 @@ void readListStatik(ListStatik *l);
 /* 2. Jika 0 < n <= CAPACITY; Lakukan n kali: 
           Baca elemen mulai dari indeks 0 satu per satu diakhiri enter */
 /*    Jika n = 0; hanya terbentuk List kosong */
-void printListStatik(ListStatik l);
+void printListPengguna(ListPengguna l);
 /* Proses : Menuliskan isi List dengan traversal, List ditulis di antara kurung 
    siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
    karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
@@ -96,7 +108,7 @@ void printListStatik(ListStatik l);
 
 /* ********** OPERATOR ARITMATIKA ********** */
 /* *** Aritmatika List : Penjumlahan, pengurangan, perkalian, ... *** */
-ListStatik plusMinusListStatik(ListStatik l1, ListStatik l2, boolean plus);
+ListPengguna plusMinusListPengguna(ListPengguna l1, ListPengguna l2, boolean plus);
 /* Prekondisi : l1 dan l2 berukuran sama dan tidak kosong */
 /* Jika plus = true, mengirimkan  l1+l2, yaitu setiap elemen l1 dan l2 pada 
        indeks yang sama dijumlahkan */
@@ -105,58 +117,58 @@ ListStatik plusMinusListStatik(ListStatik l1, ListStatik l2, boolean plus);
 
 /* ********** OPERATOR RELASIONAL ********** */
 /* *** Operasi pembandingan List: *** */
-boolean isListStatikEqual(ListStatik l1, ListStatik l2);
+boolean isListPenggunaEqual(ListPengguna l1, ListPengguna l2);
 /* Mengirimkan true jika l1 sama dengan l2 yaitu jika ukuran l1 = l2 dan semua 
    elemennya sama */
 
 /* ********** SEARCHING ********** */
 /* ***  Perhatian : List boleh kosong!! *** */
-int indexOfListStatik(ListStatik l, ElType val);
+int indexOfListPengguna(ListPengguna l, Profile val);
 /* Search apakah ada elemen List l yang bernilai val */
 /* Jika ada, menghasilkan indeks i terkecil, dengan ELMT(l,i) = val */
 /* Jika tidak ada atau jika l kosong, mengirimkan IDX_UNDEF */
 /* Skema Searching yang digunakan bebas */
 
 /* ********** NILAI EKSTREM ********** */
-void extremeValuesListStatik(ListStatik l, ElType *max, ElType *min);
+void extremeValuesListPengguna(ListPengguna l, Profile *max, Profile *min);
 /* I.S. List l tidak kosong */
 /* F.S. Max berisi nilai terbesar dalam l;
         Min berisi nilai terkecil dalam l */
 
 /* ********** MENAMBAH ELEMEN ********** */
 /* *** Menambahkan elemen terakhir *** */
-void insertFirstListStatik(ListStatik *l, ElType val);
+void insertFirstListPengguna(ListPengguna *l, Profile val);
 /* Proses: Menambahkan val sebagai elemen pertama List */
 /* I.S. List l boleh kosong, tetapi tidak penuh */
 /* F.S. val adalah elemen pertama l yang baru */
 /* *** Menambahkan elemen pada index tertentu *** */
-void insertAtListStatik(ListStatik *l, ElType val, IdxType idx);
+void insertAtListPengguna(ListPengguna *l, Profile val, IdxType idx);
 /* Proses: Menambahkan val sebagai elemen pada index idx List */
 /* I.S. List l tidak kosong dan tidak penuh, idx merupakan index yang valid di l */
 /* F.S. val adalah elemen yang disisipkan pada index idx l */
 /* *** Menambahkan elemen terakhir *** */
-void insertLastListStatik(ListStatik *l, ElType val);
+void insertLastListPengguna(ListPengguna *l, Profile val);
 /* Proses: Menambahkan val sebagai elemen terakhir List */
 /* I.S. List l boleh kosong, tetapi tidak penuh */
 /* F.S. val adalah elemen terakhir l yang baru */
 
 /* ********** MENGHAPUS ELEMEN ********** */
 /* *** Menghapus elemen pertama *** */
-void deleteFirstListStatik(ListStatik *l, ElType *val);
+void deleteFirstListPengguna(ListPengguna *l, Profile *val);
 /* Proses : Menghapus elemen pertama List */
 /* I.S. List tidak kosong */
 /* F.S. val adalah nilai elemen pertama l sebelum penghapusan, */
 /*      Banyaknya elemen List berkurang satu */
 /*      List l mungkin menjadi kosong */
 /* *** Menghapus elemen pada index tertentu *** */
-void deleteAtListStatik(ListStatik *l, ElType *val, IdxType idx);
+void deleteAtListPengguna(ListPengguna *l, Profile *val, IdxType idx);
 /* Proses : Menghapus elemen pada index idx List */
 /* I.S. List tidak kosong, idx adalah index yang valid di l */
 /* F.S. val adalah nilai elemen pada index idx l sebelum penghapusan, */
 /*      Banyaknya elemen List berkurang satu */
 /*      List l mungkin menjadi kosong */
 /* *** Menghapus elemen terakhir *** */
-void deleteLastListStatik(ListStatik *l, ElType *val);
+void deleteLastListPengguna(ListPengguna *l, Profile *val);
 /* Proses : Menghapus elemen terakhir List */
 /* I.S. List tidak kosong */
 /* F.S. val adalah nilai elemen terakhir l sebelum penghapusan, */
@@ -164,7 +176,7 @@ void deleteLastListStatik(ListStatik *l, ElType *val);
 /*      List l mungkin menjadi kosong */
 
 /* ********** SORTING ********** */
-void sortListStatik(ListStatik *l, boolean asc);
+void sortListPengguna(ListPengguna *l, boolean asc);
 /* I.S. l boleh kosong */
 /* F.S. Jika asc = true, l terurut membesar */
 /*      Jika asc = false, l terurut mengecil */
